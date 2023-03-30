66d770722f2c60bd2c721bfdb8bbff8faec58d20
andreehouman <paulandremichel7@gmail.com>
2023-03-30 11:26:46 +0200

    task_andre_ehouman

--- src/service/customer_service.py
+++ src/service/customer_service.py
@@ -1,30 +1,30 @@
 import pandas as pd
 
 from src.service.customer_repository_service import CustomerRepositoryService
+from fastapi import HTTPException
 
 
 class CustomerService:
+    def __init__(self):
+        self.__customer_repository_service = CustomerRepositoryService()
 
     def register_customer(self, name: str, surname: str, age: int, mail_address: str) -> int:
-        if name is None:
-            raise Exception()
-        if len(name) == 0:
-            raise Exception()
-        if surname is None:
-            raise Exception()
-        if len(surname) == 0:
-            raise Exception()
-        if age is None:
-            raise Exception()
-        if age < 12:
-            raise Exception()
+        if name is None or len(name) == 0:
+            raise HTTPException(status_code=400, detail='this field is not valid or must be filled')
+        if surname is None or len(surname) == 0:
+            raise HTTPException(status_code=400, detail='this field is not valid or must be filled')
+        if age is None or age < 12:
+            raise HTTPException(status_code=400, detail='this field is not valid or must be superior to 12')
+        if '@' not in mail_address:
+            raise HTTPException(status_code=400, detail='this mail_address is not valid or must be filled')
+
         name = name.lower()
-        surname = name.lower()
+        surname = surname.lower()
         mail_domain = mail_address.split('@')[1]
         c = pd.DataFrame({'name': [name],
                           'surname': [surname],
                           'age': [age],
-                          'mail_domain': [mail_domain]})
-        s = CustomerRepositoryService()
-        s.register_customer(c)
-        return s.fetch_number_of_customers()
+                          'mail_domain': [mail_domain],
+                          'mail_address': [mail_address]})
+        self.__customer_repository_service.register_customer(c)
+        return self.__customer_repository_service.fetch_number_of_customers()
